3cf27086bcebe596da7d359e6c743a23f5d73228
Talib Hussain Khan <talibhk161@gmail.com>
2024-12-04 13:00:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,35 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = ()=>{
+    const option = ["rock" ,"paper","scissors"];
+     const randomIdx = Math.floor(Math.random()*3);
+     return option[randomIdx];   
+}
+
+
+
+
+
+const playGame = (userChoice) =>{
+    console.log("user Choices =" , userChoice);
+    const compChoice = genCompChoice();
+    console.log("Computer Choice =" , compChoice);
+}
+
+
+
+
+
+
+
+
+
+
 choices.forEach((choice) =>{
    choice.addEventListener("click" , ()=>{
     const userChoice = choice.getAttribute("id");
-    console.log("Button was Clicked!" , choiceId)
+    playGame(userChoice);
    })
 })
