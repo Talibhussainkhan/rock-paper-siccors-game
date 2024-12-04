528c885d4dec22e666c5e3453bfa60022b4a1725
Talib Hussain Khan <talibhk161@gmail.com>
2024-12-04 12:35:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let userScore = 0;
 let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+choices.forEach((choice) =>{
+   choice.addEventListener("click" , ()=>{
+    const userChoice = choice.getAttribute("id");
+    console.log("Button was Clicked!" , choiceId)
+   })
+})
